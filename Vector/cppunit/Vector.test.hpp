98dc8c1a39a87f0744290fa69e49d52f1be742b3
Stefan Pantic <stefan@whitestar.local>
2017-04-30 18:01:22 +0200

    Implemented more methods, reordered template parameters

--- Vector/cppunit/Vector.test.hpp
+++ Vector/cppunit/Vector.test.hpp
@@ -24,10 +24,11 @@ namespace detail
 	};
 
 	// <default element generator>
-	struct random_number_device
+	struct _rand_gen
 	{
-		random_number_device()
-			:_engine{1}
+		static std::random_device _rd;
+		_rand_gen()
+			:_engine{_rd()}
 		{}
 
 		std::random_device::result_type operator() ()
@@ -38,6 +39,8 @@ namespace detail
 		std::mt19937 _engine;
 	};
 
+	std::random_device _rand_gen::_rd{};
+
 }
 
 // <test declaration>
@@ -45,14 +48,14 @@ template <
 		typename T,
 		typename A = std::allocator<T>,
 		size_t _size = 1000000,
-		typename _BinaryPredicate = detail::_comp<T>,
-		typename _Rand = detail::random_number_device
+		typename _element_generator = detail::_rand_gen,
+		typename _BinaryPredicate = detail::_comp<T>
 		>
 class vector_test_fixture : public CppUnit::TestFixture
 {
 	public:
 		// <implement setUp() and tearDown() for the CppUnit::TestFixture to
-		//  use to initialilize and delete vars before and after each test>
+		//  use to initialilize and delete variables before and after each test>
 		void setUp();
 		void tearDown();
 	private:
@@ -71,7 +74,6 @@ class vector_test_fixture : public CppUnit::TestFixture
 		CPPUNIT_TEST(pop_back_test);
 		CPPUNIT_TEST(swap_test);
 		CPPUNIT_TEST(clear_test);
-		CPPUNIT_TEST(find_test);
 		CPPUNIT_TEST(access_operator_test);
 		CPPUNIT_TEST(iterator_test);
 		CPPUNIT_TEST(const_iterator_test);
@@ -104,7 +106,6 @@ class vector_test_fixture : public CppUnit::TestFixture
 		void pop_back_test(void);
 		void swap_test(void);
 		void clear_test(void);
-		void find_test(void);
 		void access_operator_test(void);
 		// </data access/modification>
 
@@ -124,27 +125,24 @@ class vector_test_fixture : public CppUnit::TestFixture
 		// </tester functions>
 
 		// <local variables to use durring testing>
-		// <NOTE: some fuction tmp variables may be used for testing purposes>
-		container::vector<T, A> *v1, *v2, *v3;
+		// <NOTE: some local variables may be used in functions for testing purposes>
+		container::vector<T, A> *_v1, *_v2, *_v3;
 		// </variables>
 
 };
 // </declaration>
 
-// <registration>
 
-// <convenience typedefs>
-
+// <convenience aliases>
 using def_vect = vector_test_fixture<double, std::allocator<double>, 30000000>;
 using boost_def_vect = vector_test_fixture<double, boost::pool_allocator<double>, 30000000>;
 using fast_boost_def_vect = vector_test_fixture<double, boost::fast_pool_allocator<double>, 30000000>;
+// </convenience aliases>
 
+// <registration>
 CPPUNIT_TEST_SUITE_NAMED_REGISTRATION(def_vect, "value_type=double, allocator=std::allocator<double>, size=30,000,000");
 CPPUNIT_TEST_SUITE_NAMED_REGISTRATION(boost_def_vect, "value_type=double, allocator=boost::pool_allocator<double>, size=30,000,000");
 CPPUNIT_TEST_SUITE_NAMED_REGISTRATION(fast_boost_def_vect, "value_type=double, allocator=boost::fast_pool_allocator<double>, size=30,000,000");
-
-// </convenience typedefs>
-
 // </registration>
 
 // <init functions>
@@ -152,30 +150,31 @@ template <
 		typename T,
 		typename A,
 		size_t _size,
-		typename _BinaryPredicate,
-		typename _Rand
+		typename _element_generator,
+		typename _BinaryPredicate
 		>
 void
-vector_test_fixture<T, A, _size, _BinaryPredicate, _Rand>::setUp()
+vector_test_fixture<T, A, _size, _element_generator, _BinaryPredicate>::setUp()
 {
-	v1 = new container::vector<T, A>;
-	v2 = new container::vector<T, A>(_size);
-	//v3 = new container::vector<T, A>{300000000};
+	_v1 = new container::vector<T, A>;
+	_v2 = new container::vector<T, A>(_size);
+	_v3 = nullptr;
 }
 
 template <
 		typename T,
 		typename A,
 		size_t _size,
-		typename _BinaryPredicate,
-		typename _Rand
+		typename _element_generator,
+		typename _BinaryPredicate
 		>
 void
-vector_test_fixture<T, A, _size, _BinaryPredicate, _Rand>::tearDown()
+vector_test_fixture<T, A, _size, _element_generator, _BinaryPredicate>::tearDown()
 {
-	delete v1;
-	delete v2;
-	//delete v3;
+	delete _v1;
+	delete _v2;
+	if(nullptr != _v3)
+		delete _v3;
 }
 // </init functions>
 
@@ -186,62 +185,76 @@ template <
 		typename T,
 		typename A,
 		size_t _size,
-		typename _BinaryPredicate,
-		typename _Rand
+		typename _element_generator,
+		typename _BinaryPredicate
 		>
 void
-vector_test_fixture<T, A, _size, _BinaryPredicate, _Rand>::default_constructor_test(void)
+vector_test_fixture<T, A, _size, _element_generator, _BinaryPredicate>::default_constructor_test(void)
 {
 	std::vector<T, A> std_vect(_size);
 
-	CPPUNIT_ASSERT_MESSAGE("constructor test ", _size == v2->size());
-	CPPUNIT_ASSERT_MESSAGE("constructor test", std_vect.size() == v2->size());
-	CPPUNIT_ASSERT_MESSAGE("default init test",
-			std::equal(v2->begin(), v2->end(), std_vect.begin(), _BinaryPredicate{}));
+	CPPUNIT_ASSERT_MESSAGE("constructor test - size[init]", _size == _v2->size());
+	CPPUNIT_ASSERT_MESSAGE("constructor test - size", std_vect.size() == _v2->size());
+	CPPUNIT_ASSERT_MESSAGE("constructor test - default init ",
+			std::equal(_v2->begin(), _v2->end(), std_vect.begin(), _BinaryPredicate{}));
 }
 
 template <
 		typename T,
 		typename A,
 		size_t _size,
-		typename _BinaryPredicate,
-		typename _Rand
+		typename _element_generator,
+		typename _BinaryPredicate
 		>
 void
-vector_test_fixture<T, A, _size, _BinaryPredicate, _Rand>::copy_constructor_test(void)
+vector_test_fixture<T, A, _size, _element_generator, _BinaryPredicate>::copy_constructor_test(void)
 {
-	std::generate(v2->begin(), v2->end(), _Rand{});
+	std::generate(_v2->begin(), _v2->end(), _element_generator{});
 
-	container::vector<T, A> v_copy(*v2);
-	CPPUNIT_ASSERT_MESSAGE("copy constructor", v_copy.size() == v2->size());
-	CPPUNIT_ASSERT_MESSAGE("copy constructor",
-			std::equal(v2->begin(), v2->end(), v_copy.begin(), _BinaryPredicate{}));
+	_v3 = new container::vector<T, A>{*_v2};
+	CPPUNIT_ASSERT_MESSAGE("copy constructor - size", _v3->size() == _v2->size());
+	CPPUNIT_ASSERT_MESSAGE("copy constructor - contents",
+			std::equal(_v2->begin(), _v2->end(), _v3->begin(), _BinaryPredicate{}));
 }
 
 template <
 		typename T,
 		typename A,
 		size_t _size,
-		typename _BinaryPredicate,
-		typename _Rand
+		typename _element_generator,
+		typename _BinaryPredicate
 		>
 void
-vector_test_fixture<T, A, _size, _BinaryPredicate, _Rand>::move_constructor_test(void)
+vector_test_fixture<T, A, _size, _element_generator, _BinaryPredicate>::move_constructor_test(void)
 {
-	// TODO : implement
+	_element_generator gen{};
+	std::vector<T, A> std_vect(_size);
+	for(size_t i = 0; i < _size; ++i)
+		_v2->at(i) = std_vect[i] = gen();
+
+	CPPUNIT_ASSERT_MESSAGE("move constructor - size[before move]", _v2->size() == std_vect.size());
+	CPPUNIT_ASSERT_MESSAGE("move constructor - contents[before move]",
+			std::equal(_v2->begin(), _v2->end(), std_vect.begin(), _BinaryPredicate{}));
+
+	_v3 = new container::vector<T, A>{std::move(*_v2)};
+	std::vector<T, A> std_vect_move{std::move(std_vect)};
+
+	CPPUNIT_ASSERT_MESSAGE("move constructor - size[after move]", _v3->size() == std_vect_move.size());
+	CPPUNIT_ASSERT_MESSAGE("move constructor - contents[after move]",
+			std::equal(_v3->begin(), _v3->end(), std_vect_move.begin(), _BinaryPredicate{}));
 }
 
 template <
 		typename T,
 		typename A,
 		size_t _size,
-		typename _BinaryPredicate,
-		typename _Rand
+		typename _element_generator,
+		typename _BinaryPredicate
 		>
 void
-vector_test_fixture<T, A, _size, _BinaryPredicate, _Rand>::init_list_consturctor_test(void)
+vector_test_fixture<T, A, _size, _element_generator, _BinaryPredicate>::init_list_consturctor_test(void)
 {
-	// TODO : implement
+	// TODO : think of a way to test this
 }
 // </constructors>
 
@@ -250,44 +263,58 @@ template <
 		typename T,
 		typename A,
 		size_t _size,
-		typename _BinaryPredicate,
-		typename _Rand
+		typename _element_generator,
+		typename _BinaryPredicate
 		>
 void
-vector_test_fixture<T, A, _size, _BinaryPredicate, _Rand>::copy_assignment_test(void)
+vector_test_fixture<T, A, _size, _element_generator, _BinaryPredicate>::copy_assignment_test(void)
 {
-	std::generate(v2->begin(), v2->end(), _Rand{});
+	std::generate(_v2->begin(), _v2->end(), _element_generator{});
 
-	container::vector<T, A> v_copy = *v2;
-	CPPUNIT_ASSERT_MESSAGE("copy assignment", v_copy.size() == v2->size());
-	CPPUNIT_ASSERT_MESSAGE("copy assignment",
-			std::equal(v2->begin(), v2->end(), v_copy.begin(), _BinaryPredicate{}));
+	container::vector<T, A> v_copy_assign = *_v2;
+	CPPUNIT_ASSERT_MESSAGE("copy assignment - size", v_copy_assign.size() == _v2->size());
+	CPPUNIT_ASSERT_MESSAGE("copy assignment - contents",
+			std::equal(_v2->begin(), _v2->end(), v_copy_assign.begin(), _BinaryPredicate{}));
 }
 
 template <
 		typename T,
 		typename A,
 		size_t _size,
-		typename _BinaryPredicate,
-		typename _Rand
+		typename _element_generator,
+		typename _BinaryPredicate
 		>
 void
-vector_test_fixture<T, A, _size, _BinaryPredicate, _Rand>::move_assignment_test(void)
+vector_test_fixture<T, A, _size, _element_generator, _BinaryPredicate>::move_assignment_test(void)
 {
-	// TODO : implement
+	_element_generator gen{};
+	std::vector<T, A> std_vect(_size);
+	for(size_t i = 0; i < _size; ++i)
+		_v2->at(i) = std_vect[i] = gen();
+
+	CPPUNIT_ASSERT_MESSAGE("move assignment - size[before move]", _v2->size() == std_vect.size());
+	CPPUNIT_ASSERT_MESSAGE("move constructor - contents[before move]",
+			std::equal(_v2->begin(), _v2->end(), std_vect.begin(), _BinaryPredicate{}));
+
+	container::vector<T, A> v_move_assign = std::move(*_v2);
+	std::vector<T, A> std_vect_move = std::move(std_vect);
+
+	CPPUNIT_ASSERT_MESSAGE("move constructor - size[after move]", v_move_assign.size() == std_vect_move.size());
+	CPPUNIT_ASSERT_MESSAGE("move constructor - contents[after move]",
+			std::equal(v_move_assign.begin(), v_move_assign.end(), std_vect_move.begin(), _BinaryPredicate{}));
 }
 
 template <
 		typename T,
 		typename A,
 		size_t _size,
-		typename _BinaryPredicate,
-		typename _Rand
+		typename _element_generator,
+		typename _BinaryPredicate
 		>
 void
-vector_test_fixture<T, A, _size, _BinaryPredicate, _Rand>::init_list_assignment_test(void)
+vector_test_fixture<T, A, _size, _element_generator, _BinaryPredicate>::init_list_assignment_test(void)
 {
-	// TODO : implement
+	// TODO : think of a way to test this
 }
 // </assignment>
 
@@ -296,76 +323,121 @@ template <
 		typename T,
 		typename A,
 		size_t _size,
-		typename _BinaryPredicate,
-		typename _Rand
+		typename _element_generator,
+		typename _BinaryPredicate
 		>
 void
-vector_test_fixture<T, A, _size, _BinaryPredicate, _Rand>::push_back_test(void)
+vector_test_fixture<T, A, _size, _element_generator, _BinaryPredicate>::push_back_test(void)
 {
-	// TODO : implement
+	_element_generator _rd{};
+	std::vector<T, A> std_vect;
+	for(size_t i = 0; i < _size; ++i)
+	{
+		T tmp(_rd());
+		_v1->push_back(tmp);
+		std_vect.push_back(tmp);
+	}
+
+	CPPUNIT_ASSERT_MESSAGE("push_back - size", _v1->size() == std_vect.size());
+	CPPUNIT_ASSERT_MESSAGE("push_back - contents",
+			std::equal(_v1->begin(), _v1->end(), std_vect.begin(), _BinaryPredicate{}));
 }
 
 template <
 		typename T,
 		typename A,
 		size_t _size,
-		typename _BinaryPredicate,
-		typename _Rand
+		typename _element_generator,
+		typename _BinaryPredicate
 		>
 void
-vector_test_fixture<T, A, _size, _BinaryPredicate, _Rand>::pop_back_test(void)
+vector_test_fixture<T, A, _size, _element_generator, _BinaryPredicate>::pop_back_test(void)
 {
-	// TODO : implement
+	_element_generator gen{};
+	std::vector<T, A> std_vect(_size);
+	for(size_t i = 0; i < _size; ++i)
+		_v2->at(i) = std_vect[i] = gen();
+
+	size_t pop_back_count{gen() % _size};
+	for(size_t i = 0; i < pop_back_count; ++i)
+	{
+		_v2->pop_back();
+		std_vect.pop_back();
+		CPPUNIT_ASSERT_MESSAGE("pop_back - size", _v2->size() == std_vect.size());
+	}
+
+	CPPUNIT_ASSERT_MESSAGE("push_back - contents",
+			std::equal(_v2->begin(), _v2->end(), std_vect.begin(), _BinaryPredicate{}));
 }
 
 template <
 		typename T,
 		typename A,
 		size_t _size,
-		typename _BinaryPredicate,
-		typename _Rand
+		typename _element_generator,
+		typename _BinaryPredicate
 		>
 void
-vector_test_fixture<T, A, _size, _BinaryPredicate, _Rand>::swap_test(void)
+vector_test_fixture<T, A, _size, _element_generator, _BinaryPredicate>::swap_test(void)
 {
-	// TODO : implement
+	_element_generator _rd{};
+	std::vector<T, A> std_vect1(_size), std_vect2(_size);
+	_v3 = new container::vector<T, A>{*_v2};
+	for(size_t i = 0; i < _size; ++i)
+	{
+		T tmp1(_rd()), tmp2(_rd());
+		_v2->at(i) = std_vect1[i] = tmp1;
+		_v3->at(i) = std_vect2[i] = tmp2;
+	}
+	
+	CPPUNIT_ASSERT_MESSAGE("swap - size[before swap]", _v2->size() == std_vect1.size());
+	CPPUNIT_ASSERT_MESSAGE("swap - contents[before swap]",
+			std::equal(_v2->begin(), _v2->end(), std_vect1.begin(), _BinaryPredicate{}));
+
+	CPPUNIT_ASSERT_MESSAGE("swap - size[before swap]", _v3->size() == std_vect2.size());
+	CPPUNIT_ASSERT_MESSAGE("swap - contents[before swap]",
+			std::equal(_v3->begin(), _v3->end(), std_vect2.begin(), _BinaryPredicate{}));
+
+	_v2->swap(*_v3);
+
+	CPPUNIT_ASSERT_MESSAGE("swap - size[after swap]", _v2->size() == std_vect2.size());
+	CPPUNIT_ASSERT_MESSAGE("swap - contents[after swap]",
+			std::equal(_v2->begin(), _v2->end(), std_vect2.begin(), _BinaryPredicate{}));
+
+	CPPUNIT_ASSERT_MESSAGE("swap - size[after swap]", _v3->size() == std_vect1.size());
+	CPPUNIT_ASSERT_MESSAGE("swap - contents[after swap]",
+			std::equal(_v3->begin(), _v3->end(), std_vect1.begin(), _BinaryPredicate{}));
 }
 
 template <
 		typename T,
 		typename A,
 		size_t _size,
-		typename _BinaryPredicate,
-		typename _Rand
+		typename _element_generator,
+		typename _BinaryPredicate
 		>
 void
-vector_test_fixture<T, A, _size, _BinaryPredicate, _Rand>::clear_test(void)
+vector_test_fixture<T, A, _size, _element_generator, _BinaryPredicate>::clear_test(void)
 {
-	// TODO : implement
+	std::vector<T, A> std_vect(_size);
+	CPPUNIT_ASSERT_MESSAGE("clear - size[before clear]", _v2->size() == std_vect.size());
+	CPPUNIT_ASSERT_MESSAGE("clear - size[before clear]", _size == _v2->size());
+
+	_v2->clear();
+	std_vect.clear();
+	CPPUNIT_ASSERT_MESSAGE("clear - size[after clear]", _v2->size() == std_vect.size());
+	CPPUNIT_ASSERT_MESSAGE("clear - size[after clear]", 0 == _v2->size());
 }
 
 template <
 		typename T,
 		typename A,
 		size_t _size,
-		typename _BinaryPredicate,
-		typename _Rand
+		typename _element_generator,
+		typename _BinaryPredicate
 		>
 void
-vector_test_fixture<T, A, _size, _BinaryPredicate, _Rand>::find_test(void)
-{
-	// TODO : implement
-}
-
-template <
-		typename T,
-		typename A,
-		size_t _size,
-		typename _BinaryPredicate,
-		typename _Rand
-		>
-void
-vector_test_fixture<T, A, _size, _BinaryPredicate, _Rand>::access_operator_test(void)
+vector_test_fixture<T, A, _size, _element_generator, _BinaryPredicate>::access_operator_test(void)
 {
 	// TODO : implement
 }
@@ -376,11 +448,11 @@ template <
 		typename T,
 		typename A,
 		size_t _size,
-		typename _BinaryPredicate,
-		typename _Rand
+		typename _element_generator,
+		typename _BinaryPredicate
 		>
 void
-vector_test_fixture<T, A, _size, _BinaryPredicate, _Rand>::iterator_test(void)
+vector_test_fixture<T, A, _size, _element_generator, _BinaryPredicate>::iterator_test(void)
 {
 	// TODO : implement
 }
@@ -389,11 +461,11 @@ template <
 		typename T,
 		typename A,
 		size_t _size,
-		typename _BinaryPredicate,
-		typename _Rand
+		typename _element_generator,
+		typename _BinaryPredicate
 		>
 void
-vector_test_fixture<T, A, _size, _BinaryPredicate, _Rand>::const_iterator_test(void)
+vector_test_fixture<T, A, _size, _element_generator, _BinaryPredicate>::const_iterator_test(void)
 {
 	// TODO : implement
 }
@@ -402,11 +474,11 @@ template <
 		typename T,
 		typename A,
 		size_t _size,
-		typename _BinaryPredicate,
-		typename _Rand
+		typename _element_generator,
+		typename _BinaryPredicate
 		>
 void
-vector_test_fixture<T, A, _size, _BinaryPredicate, _Rand>::reverse_iterator_test(void)
+vector_test_fixture<T, A, _size, _element_generator, _BinaryPredicate>::reverse_iterator_test(void)
 {
 	// TODO : implement
 }
@@ -415,11 +487,11 @@ template <
 		typename T,
 		typename A,
 		size_t _size,
-		typename _BinaryPredicate,
-		typename _Rand
+		typename _element_generator,
+		typename _BinaryPredicate
 		>
 void
-vector_test_fixture<T, A, _size, _BinaryPredicate, _Rand>::const_reverse_iterator_test(void)
+vector_test_fixture<T, A, _size, _element_generator, _BinaryPredicate>::const_reverse_iterator_test(void)
 {
 	// TODO : implement
 }
@@ -430,11 +502,11 @@ template <
 		typename T,
 		typename A,
 		size_t _size,
-		typename _BinaryPredicate,
-		typename _Rand
+		typename _element_generator,
+		typename _BinaryPredicate
 		>
 void
-vector_test_fixture<T, A, _size, _BinaryPredicate, _Rand>::insert_test(void)
+vector_test_fixture<T, A, _size, _element_generator, _BinaryPredicate>::insert_test(void)
 {
 	// TODO : implement
 }
@@ -443,11 +515,11 @@ template <
 		typename T,
 		typename A,
 		size_t _size,
-		typename _BinaryPredicate,
-		typename _Rand
+		typename _element_generator,
+		typename _BinaryPredicate
 		>
 void
-vector_test_fixture<T, A, _size, _BinaryPredicate, _Rand>::erase_test(void)
+vector_test_fixture<T, A, _size, _element_generator, _BinaryPredicate>::erase_test(void)
 {
 	// TODO : implement
 }
@@ -456,11 +528,11 @@ template <
 		typename T,
 		typename A,
 		size_t _size,
-		typename _BinaryPredicate,
-		typename _Rand
+		typename _element_generator,
+		typename _BinaryPredicate
 		>
 void
-vector_test_fixture<T, A, _size, _BinaryPredicate, _Rand>::range_erase_test(void)
+vector_test_fixture<T, A, _size, _element_generator, _BinaryPredicate>::range_erase_test(void)
 {
 	// TODO : implement
 }
